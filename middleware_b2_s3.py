5ae2d8a4fea693b038f08dbd27e1c104cd5e3b0f
Stephen Boyle <stephen@thirdvantage.com>
2018-10-22 21:20:19 -0400

    add s3 logic

--- middleware_b2_s3.py
+++ middleware_b2_s3.py
@@ -3,6 +3,9 @@ Generate authorized backblaze B2 URLs for your munki repo
 This module is using munki middleware
 https://github.com/munki/munki/wiki/Middleware
 
+I have bundled in s3 handling as well so this can be dual purpose
+Credit to: https://github.com/munki/munki/wiki/Middleware
+
 Influenced heavilly by the other great middleware examples!
     - https://github.com/AaronBurchfield/CloudFront-Middleware
     - https://github.com/waderobson/gcs-auth
@@ -15,6 +18,8 @@ import datetime
 import json
 import base64
 import urllib2
+import hashlib
+import hmac
 from string import maketrans
 from urlparse import urlparse
 from Foundation import CFPreferencesCopyAppValue
@@ -23,9 +28,103 @@ from Foundation import CFPreferencesAppSynchronize
 from Foundation import kCFPreferencesAnyUser
 from Foundation import kCFPreferencesCurrentHost
 
-__version__ = '1.2'
+__version__ = '1.2b'
 
 BUNDLE = 'ManagedInstalls'
+METHOD = 'GET'
+SERVICE = 's3'
+
+
+"""
+S3
+"""
+
+def pref(pref_name):
+    """Return a preference. See munkicommon.py for details
+    """
+    pref_value = CFPreferencesCopyAppValue(pref_name, BUNDLE)
+    return pref_value
+
+
+ACCESS_KEY = pref('AccessKey')
+SECRET_KEY = pref('SecretKey')
+REGION = pref('Region')
+S3_ENDPOINT = pref('S3Endpoint') or 's3.amazonaws.com'
+
+
+def sign(key, msg):
+    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+
+def get_signature_key(key, datestamp, region, service):
+    kdate = sign(('AWS4' + key).encode('utf-8'), datestamp)
+    kregion = sign(kdate, region)
+    kservice = sign(kregion, service)
+    ksigning = sign(kservice, 'aws4_request')
+    return ksigning
+
+
+def uri_from_url(url):
+    parse = urlparse(url)
+    return parse.path
+
+
+def host_from_url(url):
+    parse = urlparse(url)
+    return parse.hostname
+
+
+def s3_auth_headers(url):
+    """
+    Returns a dict that contains all the required header information.
+    Each header is unique to the url requested.
+    """
+    # Create a date for headers and the credential string
+    time_now = datetime.datetime.utcnow()
+    amzdate = time_now.strftime('%Y%m%dT%H%M%SZ')
+    datestamp = time_now.strftime('%Y%m%d') # Date w/o time, used in credential scope
+    uri = uri_from_url(url)
+    host = host_from_url(url)
+    canonical_uri = uri
+    canonical_querystring = ''
+    canonical_headers = 'host:{}\nx-amz-date:{}\n'.format(host, amzdate)
+    signed_headers = 'host;x-amz-date'
+    payload_hash = hashlib.sha256('').hexdigest()
+    canonical_request = '{}\n{}\n{}\n{}\n{}\n{}'.format(METHOD,
+                                                        canonical_uri,
+                                                        canonical_querystring,
+                                                        canonical_headers,
+                                                        signed_headers,
+                                                        payload_hash)
+
+    algorithm = 'AWS4-HMAC-SHA256'
+    credential_scope = '{}/{}/{}/aws4_request'.format(datestamp, REGION, SERVICE)
+    hashed_request = hashlib.sha256(canonical_request).hexdigest()
+    string_to_sign = '{}\n{}\n{}\n{}'.format(algorithm,
+                                             amzdate,
+                                             credential_scope,
+                                             hashed_request)
+
+
+    signing_key = get_signature_key(SECRET_KEY, datestamp, REGION, SERVICE)
+    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+
+    authorization_header = ("{} Credential={}/{},"
+                            " SignedHeaders={}, Signature={}").format(algorithm,
+                                                                      ACCESS_KEY,
+                                                                      credential_scope,
+                                                                      signed_headers,
+                                                                      signature)
+
+    headers = {'x-amz-date': amzdate,
+               'x-amz-content-sha256': payload_hash,
+               'Authorization': authorization_header}
+    return headers
+
+
+"""
+B2
+"""
 
 def path_and_bucket(url):
     parse = urlparse(url)
@@ -171,11 +270,16 @@ def b2_url_builder(url):
     return b2_url, HEADERS
 
 def process_request_options(options):
-    """Return an authorized URL for b2."""
+    """Return an authorized URL"""
 
+    """Process B2"""
     if '://b2/' in options['url']:
         options['url'], HEADERS = b2_url_builder(options['url'])
         if HEADERS:
             options['additional_headers'].update(HEADERS)
+    """Process S3"""
+    if S3_ENDPOINT in options['url']:
+        headers = s3_auth_headers(options['url'])
+        options['additional_headers'].update(headers)
 
     return options
